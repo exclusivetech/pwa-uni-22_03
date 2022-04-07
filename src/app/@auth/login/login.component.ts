4fe07532516177963b27bcafbc7b57d1b9c5b9e9
daniele <d.r@exclusivetech.ch>
2022-04-07 16:11:52 +0000

    gRPC-web proxy completo

--- src/app/@auth/login/login.component.ts
+++ src/app/@auth/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HelloReply, HelloRequest } from 'src/app/@grpc/helloworld_pb';
+import { GreeterClient, ServiceError } from 'src/app/@grpc/helloworld_pb_service';
 import { JwtTokenAuthService } from 't-auth';
 
 @Component({
@@ -8,9 +10,14 @@ import { JwtTokenAuthService } from 't-auth';
 })
 export class LoginComponent implements OnInit {
 
+  private grpc: GreeterClient;
+
   constructor(
-    private jwta: JwtTokenAuthService
-  ) { }
+    private jwta: JwtTokenAuthService,
+  ) {
+    this.grpc = new GreeterClient(
+      'http://localhost:4200');
+  }
 
   ngOnInit(): void {
   }
@@ -33,6 +40,20 @@ export class LoginComponent implements OnInit {
   loginNO() {
     console.log('debug login NO');
     this.jwta.setDebug(false);
+  }
+
+  /**
+   * prova chiamata gRPC vedere grpc-cli-22.04
+   */
+  grpcCall() {
+    const request = new HelloRequest();
+    request.setName('pedrito');
+
+    this.grpc.sayHello(request, (error: ServiceError | null, response: HelloReply | null) => {
+      console.log('arriva::::', error, response, response?.getMessage());
+    });
+
+
   }
 
 }
